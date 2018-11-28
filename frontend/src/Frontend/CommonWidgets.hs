30a66a05278cfccf5759ac86381a661b47b0c2d4
Divam <dfordivam@gmail.com>
2018-11-28 09:26:21 +0900

    Update CommonWidgets.hs
    add rel=noopener attr

--- frontend/src/Frontend/CommonWidgets.hs
+++ frontend/src/Frontend/CommonWidgets.hs
@@ -6,4 +6,4 @@ import Data.Text (Text)
 
 extLink :: DomBuilder t m => Text -> m a -> m a
 extLink href m =
-  elAttr "a" ("href" =: href <> "target" =: "_blank") $ m
+  elAttr "a" ("href" =: href <> "target" =: "_blank" <> "rel" =: "noopener") $ m
