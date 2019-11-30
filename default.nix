fa6ac5056a453ec4b5c0ddc876e195ca5344e85f
Ali Abrar <aliabrar@gmail.com>
2019-11-29 23:27:58 -0500

    Use the reunified calculator-tutorial

--- default.nix
+++ default.nix
@@ -16,7 +16,6 @@ in {
 
   packages = {
     obelisk-google-analytics = hackGet deps/obelisk-google-analytics;
-    tutorial = tutorialSrc + "/tutorial";
     # TODO make overlay or something in that repo
     mmark = hackGet ./deps/mmark;
     modern-uri = hackGet ./deps/modern-uri;
@@ -29,6 +28,20 @@ in {
     unliftio = dontCheck super.unliftio;
     yaml = dontCheck super.yaml;
     modern-uri = doJailbreak super.modern-uri;
+    # TODO: Find a better way to combine obelisk apps:
+    tutorial-frontend = self.callCabal2nix "tutorial-frontend" (pkgs.runCommand "tutorial-import" {} ''
+      set -eux
+      mkdir tutorial-frontend
+      cp -rL "${tutorialSrc + "/frontend"}"/* tutorial-frontend/
+      mv tutorial-frontend/frontend.cabal tutorial-frontend/tutorial-frontend.cabal
+      substituteInPlace tutorial-frontend/tutorial-frontend.cabal \
+        --replace "name: frontend" "name: tutorial-frontend" \
+        --replace "executable frontend" "executable tutorial-frontend${"\n"}  buildable: false" \
+        --replace ", frontend" ", tutorial-frontend" \
+        --replace "exposed-modules: Frontend" "exposed-modules: "
+      mkdir $out
+      cp -rL tutorial-frontend/* $out/
+    '') {};
   };
 
 })
