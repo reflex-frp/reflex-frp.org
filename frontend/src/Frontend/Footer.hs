9342bcf1c430650c30fa3d9a2dda6f00d18690d5
Sean Chalmers <sean.chalmers@obsidian.systems>
2022-08-12 10:18:50 +0100

    Remove twitter link from footer

--- frontend/src/Frontend/Footer.hs
+++ frontend/src/Frontend/Footer.hs
@@ -15,9 +15,6 @@ import Frontend.CommonWidgets
 footer :: (Analytics t m, DomBuilder t m) => m ()
 footer = do
   el "nav" $ elClass "ul" "social" $ do
-    el "li" $ extLinkAttr ("title" =: "Twitter") "https://twitter.com/reflexfrp" $ do
-      elClass "i" "icon-twitter" blank
-      text "@reflexfrp"
     el "li" $ extLinkAttr ("title" =: "Reddit") "https://reddit.com/r/reflexfrp" $ do
       elClass "i" "fa-solid fa-brands fa-reddit" blank
       text "r/reflexfrp"
